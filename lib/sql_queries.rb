5f9361035abcad185c0654ae66a5356d4563b929
Linda Chang <lindachang186@gmail.com>
2019-01-12 20:14:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -50,7 +50,7 @@ WHERE projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.name, SUM(pledges.amount)
+"SELECT projects.category, SUM(pledges.amount)
 FROM pledges
 LEFT OUTER JOIN Projects
 On pledges.project_id = projects.id
